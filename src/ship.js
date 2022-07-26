7e800420c538364213e8062973018932613f38c4
Ben Casson <bcasson11@gmail.com>
2022-07-25 23:53:44 -0500

    Add createShip factory function

---
+++ src/ship.js
@@ -0,0 +1,8 @@
+export const createShip = (length) => {
+    //array of Maps, each Map contains an 'x' key and a 'y' key
+    const coordinates = [];
+    function setCoordinates() {
+
+    }
+    return { length, coordinates, setCoordinates };
+}
