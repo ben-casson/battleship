b6c78ef8f790d2af9d150e549a442d6dd7322053
BenCasson <bcasson11@gmail.com>
2022-07-26 10:28:04 -0500

    Add function to mark ship as hit by enemy

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,12 @@
 export const createShip = (length) => {
-    //array of Maps, each Map contains an 'x' key and a 'y' key
+    //array of Maps, each Map contains an 'x' key, a 'y' key, and an 'is hit' key
     const coordinates = [];
-    function setCoordinates() {
-
+    function hit(xCoordinate, yCoordinate) {
+        for (let map of coordinates) {
+            if (map.get('x') === xCoordinate && map.get('y') === yCoordinate) {
+                map.set('is hit', true);
+            }
+        }
     }
-    return { length, coordinates, setCoordinates };
+    return { length, coordinates, hit };
 }
