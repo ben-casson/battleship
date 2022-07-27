deef75050bcbc839741ca5780a970f78e0a7991a
BenCasson <bcasson11@gmail.com>
2022-07-27 10:12:41 -0500

    Add 'has ship' and 'attacked' values to coordinate

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,7 +4,7 @@ export const createGameboard = () => {
         for (let i = 0; i < 10; i++) {
             const rowArray = [];
             for (let j = 0; j < 10; j++) {
-                rowArray.push('unattacked');
+                rowArray.push(new Map().set('has ship', false).set('attacked', false));
             }
             this.coordinatesGrid.push(rowArray);
         }
