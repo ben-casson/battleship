f5a4276f005516e76fe6f7a966acb9a8b02958af
BenCasson <bcasson11@gmail.com>
2022-07-27 11:25:24 -0500

    Allow ship objects to be added to gameboard grid

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,7 +4,7 @@ export const createGameboard = () => {
         for (let i = 0; i < 10; i++) {
             const rowArray = [];
             for (let j = 0; j < 10; j++) {
-                rowArray.push(new Map().set('has ship', false).set('attacked', false));
+                rowArray.push(new Map().set('ship', 'none').set('attacked', false));
             }
             this.coordinatesGrid.push(rowArray);
         }
@@ -23,11 +23,15 @@ export const createGameboard = () => {
             for (let j = 0; j < 10; j++) {
                 for (let k = 0; k < shipObject.coordinates.length; k++) {
                     if (shipObject.coordinates[k].get('x') === j && shipObject.coordinates[k].get('y') === i) {
-                        this.coordinatesGrid[i][j].set('has ship', true);
+                        this.coordinatesGrid[i][j].set('ship', shipObject);
                     }
                 }
             }
         }
+    }
+
+    function receiveAttack(xCoordinate, yCoordinate) {
+
     }
 
     return { coordinatesGrid, generateCoordinatesGrid, shipsCoordinatesList, addShipCoordinatesToList,
