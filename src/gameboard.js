3ccb45cb1c37cdb666b27d461d7125fbbae30da5
Ben Casson <bcasson11@gmail.com>
2022-07-26 21:45:46 -0500

    Add generateCoordinatesGrid function

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,14 @@
 export const createGameboard = () => {
-    
+    const coordinatesGrid = [];
+    function generateCoordinatesGrid() {
+        for (let i = 0; i < 10; i++) {
+            const rowArray = [];
+            for (let j = 0; j < 10; j++) {
+                rowArray.push('unattacked');
+            }
+            this.coordinatesGrid.push(rowArray);
+        }
+    }
+
+    return { coordinatesGrid, generateCoordinatesGrid };
 }
