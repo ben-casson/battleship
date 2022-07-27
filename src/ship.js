16657b2406a8670b2232f140db314d3b98760d69
BenCasson <bcasson11@gmail.com>
2022-07-27 10:44:17 -0500

    Edit comment

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,5 @@
 export const createShip = (length) => {
-    //array of Maps, each Map contains an 'x' key, a 'y' key, and a 'hit' key
+    //array of Maps, each Map contains an 'x' coordinate key, a 'y' coordinate key, and a 'hit' key
     const coordinates = [];
     function hit(xCoordinate, yCoordinate) {
         for (let map of this.coordinates) {
