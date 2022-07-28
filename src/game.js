fde5fd5e5fc816568d351af97c6f8e5df269e01e
BenCasson <bcasson11@gmail.com>
2022-07-28 10:41:14 -0500

    Create all 10 ships

--- src/game.js
+++ src/game.js
@@ -1,9 +1,22 @@
 import { createGameboard } from "./models/gameboard";
 import { createShip } from "./models/ship";
 
+const friendlyCarrier = createShip(5);
+const friendlyBattleship = createShip(4);
+const friendlyCruiser1 = createShip(3);
+const friendlyCruiser2 = createShip(3);
+const friendlyDestroyer = createShip(5);
+
+const enemyCarrier = createShip(5);
+const enemyBattleship = createShip(4);
+const enemyCruiser1 = createShip(3);
+const enemyCruiser2 = createShip(3);
+const enemyDestroyer = createShip(5);
+
 export function playGame() {
     const friendlyGameboard = createGameboard();
     friendlyGameboard.generateCoordinatesGrid();
     
     //dynamically generate grid ui (import from /views)
 }
+
