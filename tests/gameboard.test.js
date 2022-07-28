7a06b5b84d10f8a76e8a53a6aa4d337e4797c9b9
Ben Casson <bcasson11@gmail.com>
2022-07-27 19:53:39 -0500

    Test if gameboard knows all ships are sunk

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -15,7 +15,7 @@ describe('public gameboard methods', () => {
         destroyer = createShip(2);
         cruiser = createShip(3);
         destroyer.coordinates = [new Map().set('x', 0).set('y', 0).set('hit', false),
-                                  new Map().set('x', 0).set('y', 1).set('hit', false)];
+                                 new Map().set('x', 0).set('y', 1).set('hit', false)];
         cruiser.coordinates = [new Map().set('x', 9).set('y', 0).set('hit', false),
                                new Map().set('x', 9).set('y', 1).set('hit', false),
                                new Map().set('x', 9).set('y', 2).set('hit', false)];
@@ -39,6 +39,7 @@ describe('public gameboard methods', () => {
         expect(friendlyGameboard.shipsCoordinatesList[0][1].get('x')).toBe(0);
         expect(friendlyGameboard.shipsCoordinatesList[0][1].get('y')).toBe(1);
         expect(friendlyGameboard.shipsCoordinatesList[0][1].get('hit')).toBeFalsy();
+        expect(enemyGameboard.shipsCoordinatesList.length).toBe(0);
     });
 
     test('ship is added to gameboard coordinates grid', () => {
@@ -69,5 +70,18 @@ describe('public gameboard methods', () => {
         expect(friendlyGameboard.coordinatesGrid[1][0].get('attacked')).toBeFalsy();
         expect(friendlyGameboard.coordinatesGrid[5][8].get('attacked')).toBeFalsy();
         expect(enemyGameboard.coordinatesGrid[0][0].get('attacked')).toBeFalsy();
+    });
+
+    test('all ships have been sunk', () => {
+        friendlyGameboard.addShipToGameboard(destroyer);
+        friendlyGameboard.addShipToGameboard(cruiser);
+        expect(friendlyGameboard.allShipsAreSunk()).toBeFalsy();
+        friendlyGameboard.receiveAttack(0, 0);
+        friendlyGameboard.receiveAttack(0, 1);
+        expect(friendlyGameboard.allShipsAreSunk()).toBeFalsy();
+        friendlyGameboard.receiveAttack(9, 0);
+        friendlyGameboard.receiveAttack(9, 1);
+        friendlyGameboard.receiveAttack(9, 2);
+        expect(friendlyGameboard.allShipsAreSunk()).toBeTruthy();
     });
 });
