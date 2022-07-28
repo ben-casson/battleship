bf02947c22ef41ad1d8b51106158d402dba02f22
Ben Casson <bcasson11@gmail.com>
2022-07-27 20:15:28 -0500

    Separate private and public methods

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,29 +3,51 @@ export const createGameboard = () => {
     const shipsList = [];
     const shipsCoordinatesList = [];
 
+    //Public Methods---------------------------------------------------------------
+
     function generateCoordinatesGrid() {
         for (let row = 0; row < 10; row++) {
             const rowArray = [];
             for (let column = 0; column < 10; column++) {
                 rowArray.push(new Map().set('ship', 'none').set('attacked', false));
             }
-            //this.
             coordinatesGrid.push(rowArray);
         }
     }
 
+    function addShipToGameboard(shipObject) {
+        addShipToShipsList(shipObject);
+        addShipToCoordinatesGrid(shipObject);
+        addShipCoordinatesToCoordinatesList(shipObject.coordinates);
+    }
+    
+    function receiveAttack(xCoordinate, yCoordinate) {
+        let currentCoordinate = coordinatesGrid.at(yCoordinate).at(xCoordinate);
+        if (currentCoordinate.get('ship') !== 'none') {
+            currentCoordinate.get('ship').hit(xCoordinate, yCoordinate);
+        }
+        currentCoordinate.set('attacked', true);
+    }
+    
+    function allShipsAreSunk() {
+        for (let ship of this.shipsList) {
+            if (!ship.isSunk()) return false;
+        }
+        return true;
+    }
+
+    //Private Methods--------------------------------------------------------------
+
     function addShipCoordinatesToCoordinatesList(shipCoordinatesArray) {
-        //this.
         shipsCoordinatesList.push(shipCoordinatesArray);
     }
 
-    //call upon tile click to place ship, after ship created, gameboard created, etc.
     function addShipToCoordinatesGrid(shipObject) {
         for(let row = 0; row < 10; row++) {
             for (let column = 0; column < 10; column++) {
                 for (let i = 0; i < shipObject.coordinates.length; i++) {
-                    if (shipObject.coordinates[i].get('x') === column && shipObject.coordinates[i].get('y') === row) {
-                        //this.
+                    if (shipObject.coordinates[i].get('x') === column 
+                        && shipObject.coordinates[i].get('y') === row) {
                         coordinatesGrid[row][column].set('ship', shipObject);
                     }
                 }
@@ -34,32 +56,9 @@ export const createGameboard = () => {
     }
 
     function addShipToShipsList(shipObject) {
-        //this.
         shipsList.push(shipObject);
     }
 
-    function addShipToGameboard(shipObject) {
-        addShipToShipsList(shipObject);
-        addShipToCoordinatesGrid(shipObject);
-        addShipCoordinatesToCoordinatesList(shipObject.coordinates);
-    }
-
-    function receiveAttack(xCoordinate, yCoordinate) {
-        //                      this.
-        let currentCoordinate = coordinatesGrid.at(yCoordinate).at(xCoordinate);
-        if (currentCoordinate.get('ship') !== 'none') {
-            currentCoordinate.get('ship').hit(xCoordinate, yCoordinate);
-        }
-        currentCoordinate.set('attacked', true);
-    }
-
-    function allShipsAreSunk() {
-        for (let ship of this.shipsList) {
-            if (!ship.isSunk()) return false;
-        }
-        return true;
-    }
-
-    return { coordinatesGrid, generateCoordinatesGrid, shipsCoordinatesList, addShipToGameboard,
-             shipsList, receiveAttack, allShipsAreSunk };
+    return { coordinatesGrid, generateCoordinatesGrid, shipsCoordinatesList, 
+             addShipToGameboard, shipsList, receiveAttack, allShipsAreSunk };
 }
