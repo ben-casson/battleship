904ee64e7d3afb236fadb8317025095264755175
Ben Casson <bcasson11@gmail.com>
2022-07-30 19:21:38 -0500

    Put logic for placing ships in separate function

--- src/game.js
+++ src/game.js
@@ -6,36 +6,39 @@ import { changeShipOrientation,
          friendlyGameboardDisplay,
          addShipToGameboardDisplay} from "./views.js";
 
-export function playGame() {
-    const friendlyCarrier = createShip(5);
-    const friendlyBattleship = createShip(4);
-    const friendlyCruiser1 = createShip(3);
-    const friendlyCruiser2 = createShip(3);
-    const friendlyDestroyer = createShip(2);
-    const friendlyShipsList = [friendlyCarrier, friendlyBattleship, friendlyCruiser1, 
-                               friendlyCruiser2, friendlyDestroyer]
-    
-    const enemyCarrier = createShip(5);
-    const enemyBattleship = createShip(4);
-    const enemyCruiser1 = createShip(3);
-    const enemyCruiser2 = createShip(3);
-    const enemyDestroyer = createShip(2);
-    
-    const friendlyGameboard = createGameboard();
-    friendlyGameboard.generateCoordinatesGrid();
-    
-    const enemyGameboard = createGameboard();
-    enemyGameboard.generateCoordinatesGrid();
+const friendlyCarrier = createShip(5);
+const friendlyBattleship = createShip(4);
+const friendlyCruiser1 = createShip(3);
+const friendlyCruiser2 = createShip(3);
+const friendlyDestroyer = createShip(2);
+export const friendlyShipsList = [friendlyCarrier, friendlyBattleship, friendlyCruiser1, 
+                                    friendlyCruiser2, friendlyDestroyer]
 
+const enemyCarrier = createShip(5);
+const enemyBattleship = createShip(4);
+const enemyCruiser1 = createShip(3);
+const enemyCruiser2 = createShip(3);
+const enemyDestroyer = createShip(2);
+
+export const friendlyGameboard = createGameboard();
+friendlyGameboard.generateCoordinatesGrid();
+
+const enemyGameboard = createGameboard();
+enemyGameboard.generateCoordinatesGrid();
+
+function letUserPlaceShips(tilesList) {
     changeShipOrientation();
-
-    displayGameboardTiles(friendlyGameboardDisplay, 'friendly-gameboard-display');
-
-    const tilesList = [...document.querySelectorAll('.friendly-gameboard-display-tile')];
     friendlyGameboardDisplay.addEventListener('mouseleave', () => {
         tilesList.forEach(tile => tile.classList.remove('ship-preview'));
     });
-    showPotentialShipOnMouseover(friendlyCarrier, tilesList);
-    addShipToGameboardDisplay(friendlyCarrier, tilesList);
+    showPotentialShipOnMouseover(tilesList);
+    addShipToGameboardDisplay(tilesList); 
+}
+
+export function playGame() {
+    displayGameboardTiles(friendlyGameboardDisplay, 'friendly-gameboard-display');
+    const tilesList = [...document.querySelectorAll('.friendly-gameboard-display-tile')];
+    
+    letUserPlaceShips(tilesList);
 }
 
